57ac26bd8a9534e2604740de32cde6506121c4fa
Igor_ <igrmartoli@gmail.com>
2022-03-27 21:36:07 -0300

    first

--- src/main/java/Aluno.java
+++ src/main/java/Aluno.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author igor_
- */
 public class Aluno extends Pessoa{
     private int periodo;
     private int ano;
